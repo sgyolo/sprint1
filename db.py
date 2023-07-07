acdd6b81dd3d75729fba1ebaf5b7db89193ebf4b
John Doe <john.doe@example.com>
2023-07-07 20:22:17 +0300

    Created try_to_add_crossing method

--- db.py
+++ db.py
@@ -1,8 +1,11 @@
-import asyncio
+from typing import TYPE_CHECKING
 
-from tortoise import Tortoise
+if TYPE_CHECKING:
+    from fastapi import FastAPI
+
 from tortoise.contrib.fastapi import register_tortoise
 from os import getenv
+from models import Crossing, User, Coords, Level, Image, Status
 
 FSTR_DB_HOST = getenv("FSTR_DB_HOST")
 FSTR_DB_PORT = getenv("FSTR_DB_PORT", "5432")
@@ -13,13 +16,33 @@ FSTR_DB_NAME = getenv("FSTR_DB_NAME", "pereval")
 
 DB_URL = f"postgres://{FSTR_DB_LOGIN}:{FSTR_DB_PASS}@{FSTR_DB_HOST}:{FSTR_DB_PORT}/"
 
-async def init():
-
-
 class CrossingsDB:
     def __init__(self, app: "FastAPI",  db_name: str):
-        self.db_name = db_name
-        register_tortoise(app, modules={"models": ["models"]}, db_url= DB_URL + db_name)
+        self.db_url = DB_URL + db_name
+        register_tortoise(app, modules={"models": ["models"]}, db_url= self.db_url, generate_schemas=True)
 
-    async def add_crossing(self, payload):
-        ...
+
+    async def try_to_add_crossing(self, payload: dict[str, ...]) -> bool | Crossing:
+        try:
+            if not (user := await User.get_or_none(email=payload["user"]["email"])):
+                user = await User.create(**payload["user"])
+            level = await Level.create(**payload["level"])
+            coords = await Coords.create(**payload["coords"])
+        except KeyError:
+            return False
+
+        images = payload.pop("images", [])
+
+        payload["level"] = level
+        payload["coords"] = coords
+        payload["user"] = user
+
+        payload["status"] = Status.NEW
+
+        crossing = await Crossing.create(**payload)
+        for image in images:
+            await Image.create(**image, crossing=crossing)
+
+        return crossing
+
+
