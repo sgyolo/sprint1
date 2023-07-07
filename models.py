6083face4396cbc45d66b6a2b2078b0c1ab82d70
vovsss <79599133+vovsss@users.noreply.github.com>
2023-07-07 19:57:40 +0300

    Updated models

--- models.py
+++ models.py
@@ -1,29 +1,36 @@
-from tortoise.models import Model
-from tortoise import fields
-import tortoise.models
+from tortoise.models import Model, MetaInfo
+from tortoise import fields, Tortoise
+from tortoise.contrib.pydantic import pydantic_model_creator
 
 from enum import StrEnum
 
-class Area(Model):
+
+class PydanticExlcudeIDModel(Model):
+    class PydanticMeta:
+        exclude = ("id",)
+
+
+class Area(PydanticExlcudeIDModel):
     id = fields.IntField(pk=True)
     parent_id = fields.ForeignKeyField("models.Area")
 
 
-class ActivityType(Model):
+class ActivityType(PydanticExlcudeIDModel):
     id = fields.IntField(pk=True)
     title = fields.CharField(max_length=15)
 
 
+class Image(PydanticExlcudeIDModel):
+    class PydanticMeta:
+        exclude = ("id", "date_added")
 
-class CrossingImage(Model):
-    id = fields.IntField(pk=True)
-    crossing = fields.ForeignKeyRelation("models.Crossing")
-    image = fields.ForeignKeyRelation("models.Image")
-
-class Image(Model):
     id = fields.IntField(pk=True)
     date_added = fields.DatetimeField(auto_now_add=True)
-    img = fields.BinaryField()
+    data = fields.BinaryField()
+    title = fields.CharField(max_length=30)
+
+    crossing = fields.ForeignKeyField(model_name="models.Crossing", related_name="images")
+
 
 class Status(StrEnum):
     NEW = "new"
@@ -31,28 +38,16 @@ class Status(StrEnum):
     ACCEPTED = "accepted"
     REJECTED = "rejected"
 
-class Level(Model):
+
+class Level(PydanticExlcudeIDModel):
+    id = fields.IntField(pk=True)
     winter = fields.CharField(max_length=10)
     spring = fields.CharField(max_length=10)
     summer = fields.CharField(max_length=10)
     autumn = fields.CharField(max_length=10)
 
-class Crossing(Model):
-    id = fields.IntField(pk=True)
 
-    user = fields.ForeignKeyField("models.User", related_name="crossings")
-    connect = fields.CharField(max_length=10)
-
-    beauty_title = fields.CharField(max_length=30)
-    title = fields.CharField(max_length=30)
-
-    add_time = fields.DatetimeField(auto_now_add=True)
-
-    level = fields.OneToOneField("models.Level")
-    coord_id = fields.OneToOneField("models.Coords", related_name="crossing")
-
-
-class User(Model):
+class User(PydanticExlcudeIDModel):
     id = fields.IntField(pk=True)
     email = fields.CharField(unique=True, max_length=30)
     phone = fields.CharField(max_length=14)
@@ -61,8 +56,29 @@ class User(Model):
     otc = fields.CharField(max_length=30)
 
 
-class Coords(Model):
-    coord_id = fields.IntField(pk=True)
+class Crossing(PydanticExlcudeIDModel):
+    id = fields.IntField(pk=True)
+
+    user = fields.ForeignKeyField("models.User", related_name="crossings")
+
+    connect = fields.CharField(max_length=10)
+    beauty_title = fields.CharField(max_length=30)
+    title = fields.CharField(max_length=30)
+    other_titles = fields.CharField(max_length=30)
+
+    add_time = fields.DatetimeField()
+
+    level = fields.OneToOneField("models.Level", related_name="crossing")
+    coords = fields.OneToOneField("models.Coords", related_name="crossing")
+
+
+class Coords(PydanticExlcudeIDModel):
+    id = fields.IntField(pk=True)
     latitude = fields.FloatField()
     longitude = fields.FloatField()
     height = fields.IntField()
+
+
+Tortoise.init_models(["models"], "models")
+CrossingPydantic = pydantic_model_creator(Crossing)
+
